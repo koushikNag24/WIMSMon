e4918bf3a93499f4a61d8fb9a25d8e3e3c003186
Koushik <koushik.nag@yahoo.com>
2022-12-26 18:16:49 +0530

    Initialized project

---
+++ src/main/java/org/isro/istrac/nsa/inoctf/App.java
@@ -0,0 +1,13 @@
+package org.isro.istrac.nsa.inoctf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/isro/istrac/nsa/inoctf/config/Config.java
@@ -0,0 +1,14 @@
+package org.isro.istrac.nsa.inoctf.config;
+
+import java.util.List;
+
+public class Config {
+    private String name;
+    private int age;
+    private List<String> messages;
+
+    public Config loadConfiguration(){
+
+        return null;
+    }
+}

2fd78d4aa07ef2588207a2af635874a4765b158f
Prapti Mittal <prapti_mittal@istrac.gov.in>
2022-12-27 00:39:08 +0530

    Added config feature

--- src/main/java/org/isro/istrac/nsa/inoctf/App.java
+++ src/main/java/org/isro/istrac/nsa/inoctf/App.java
@@ -1,13 +1,27 @@
 package org.isro.istrac.nsa.inoctf;
 
+import lombok.extern.slf4j.Slf4j;
+import org.isro.istrac.nsa.inoctf.config.Config;
+import org.isro.istrac.nsa.inoctf.exception.ConfigurationException;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
+        Config config;
+        try {
+            config = Config.loadConfiguration().orElseThrow(() -> new ConfigurationException("Configuration Load Error"));
+        } catch (IOException | ConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(config);
+
     }
 }
--- src/main/java/org/isro/istrac/nsa/inoctf/config/Config.java
+++ src/main/java/org/isro/istrac/nsa/inoctf/config/Config.java
@@ -1,14 +1,31 @@
 package org.isro.istrac.nsa.inoctf.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Optional;
+@Setter
+@Getter
+@ToString
 public class Config {
-    private String name;
-    private int age;
-    private List<String> messages;
 
-    public Config loadConfiguration(){
+    private static final String configFile="/home/lgmdev/IdeaProjects/WIMSMon/src/main/resources/config.json";
+    private  int delayTimeInSeconds;
+    private int lastUpdatedPermissibleIntervalInSeconds;
+    private List<String> systemCtlProcesses;
+    private List<String> filesToMonitor;
+    private HashMap<String,Integer> processConfig;
+    private String statusFile;
 
-        return null;
+    public static Optional<Config> loadConfiguration() throws IOException {
+        ObjectMapper objectMapper=new ObjectMapper();
+        Config config = objectMapper.readValue(new File(configFile), Config.class);
+        return Optional.ofNullable(config);
     }
 }
